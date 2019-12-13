c9c7d66f24350087fd43230f3dc603f1cfa9e93f
amarjeetrao <amarjeetrao33@gmail.com>
2019-12-13 18:08:16 +0530

    layout ready with styling

---
+++ src/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $(".nav-link").click(function(e) {
+        $('.nav-link').removeClass('sidebar-active');
+        $(this).addClass('sidebar-active');
+        $(this).parents('.show').toggleClass('show');
+    });
+});

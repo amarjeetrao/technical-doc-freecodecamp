015bf7bb4ced0e36ad5a6f04fd8a5f20ffccd641
amarjeetrao <amarjeetrao33@gmail.com>
2019-12-14 14:41:45 +0530

    layout imporvment

--- src/script.js
+++ src/script.js
@@ -1,7 +1,9 @@
-$(document).ready(function () {
-    $(".nav-link").click(function(e) {
-        $('.nav-link').removeClass('sidebar-active');
-        $(this).addClass('sidebar-active');
-        $(this).parents('.show').toggleClass('show');
-    });
+$(document).ready(function() {
+  $(".nav-link").click(function(e) {
+    $(".nav-link").removeClass("sidebar-active");
+    $(this).addClass("sidebar-active");
+    $(this)
+      .parents(".show")
+      .toggleClass("show");
+  });
 });
